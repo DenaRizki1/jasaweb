779a9920ec6ed3025e3fe81c28ef895877fdeb23
Denarizki <86529218+DenaRizki1@users.noreply.github.com>
2021-08-05 18:12:09 +0700

    Add files via upload

---
+++ tema-02/assets/javascript.js
@@ -0,0 +1,59 @@
+var countDownDate = new Date("aug 5, 2023 08:00:00").getTime();
+
+var x = setInterval(function() {
+
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var hari = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var jam = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var menit = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var detik = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("hari").innerHTML = hari + " <br> Hari ";
+  document.getElementById("jam").innerHTML = jam + " <br> Jam ";
+  document.getElementById("menit").innerHTML = menit + " <br> Menit ";
+  document.getElementById("detik").innerHTML = detik + " <br> Detik ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("hari").innerHTML = "0 <br> Hari";
+    document.getElementById("jam").innerHTML = "0 <br> jam";
+    document.getElementById("menit").innerHTML = "0 <br> Menit";
+    document.getElementById("detik").innerHTML = "0 <br> Detik";
+  }
+}, 1000);
+
+var musik = new Audio();
+        musik.src="assets/backsound.mp3";
+        musik.loop=true;
+        musik.play();
+
+        function mulaiAudio(){
+            var play = document.getElementById("play");
+            var pause = document.getElementById("pause");
+
+            play.addEventListener('click', fplay);
+            pause.addEventListener('click', fpause);
+
+            function fplay(){
+                if(musik.paused){
+                    musik.play();
+                }else{
+                    musik.pause();
+                }
+            }
+            function fpause(){
+                if(musik.paused){
+                    musik.play();
+                }else{
+                    musik.pause();
+                }
+            }
+        }
+        window.addEventListener('load', mulaiAudio);
+
+function buka(){
+    window.location = "undangan.html";
+}
