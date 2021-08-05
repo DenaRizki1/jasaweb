7691828c34f0d61363d4362c45dcea616dd5f7a2
Denarizki <86529218+DenaRizki1@users.noreply.github.com>
2021-08-05 18:14:25 +0700

    tema02

---
+++ script.js
@@ -0,0 +1,19 @@
+function price() {
+    window.location="#price";
+  };
+
+  function reguler() {
+    window.location="https://wa.wizard.id/349dac"
+  }
+
+  function premium() {
+    window.location="https://wa.wizard.id/d829dd";
+  }
+
+  function tema01() {
+    window.location= "tema-01/";
+  }
+
+  function tema02() {
+    window.location = "tema-02/";
+  }
